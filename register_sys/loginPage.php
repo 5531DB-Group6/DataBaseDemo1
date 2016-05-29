26a7277d5d344d533cbcdda57e6604f65a3a132c
CellCS <shmilysyg@gmail.com>
2016-05-29 11:56:59 -0400

    Add files via upload

---
+++ register_sys/loginPage.php
@@ -0,0 +1,12 @@
+
+<html>
+	<form action ='login.php' method='POST'>
+		<p><label>email: <input type =  'email' name= 'email'></label><br>
+		<p><label>password: <input type =  'password' name= 'password'></label><br>
+		<input type =  'submit' value= 'submit'><br>
+	</form> <p>
+	
+	<form action ='registerPage.php' method='POST'>
+		<input type = 'submit' value= 'register'><br>
+	</form> <p>
+</html>
